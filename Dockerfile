dc564cd1f2eed2b3494266b2ad75ab8a222b2a47
andrewvitrenko <avitrenko2004@gmail.com>
2023-04-23 15:26:10 +0200

    feat: added numpy and endpoint

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM python:3.11.2-alpine3.16 AS builder
 
+RUN apk update
+RUN apk --no-cache add musl-dev g++
 COPY requirements requirements
 RUN python -m venv /.venv
 ENV PATH="/.venv/bin:$PATH"
--- spaceship/routers/api.py
+++ spaceship/routers/api.py
@@ -1,4 +1,6 @@
+import numpy
 from fastapi import APIRouter
+from fastapi.responses import JSONResponse
 
 router = APIRouter()
 
@@ -6,3 +8,14 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get('/numpy')
+def get_res():
+    matrix_a = numpy.random.random_integers(0, 10, (10, 10))
+    matrix_b = numpy.random.random_integers(0, 10, (10, 10))
+    product = matrix_a.dot(matrix_b).tolist()
+    return JSONResponse(content={
+        'matrix_A': matrix_a.tolist(),
+        'matrix_B': matrix_b.tolist(),
+        'product': product,
+    })
